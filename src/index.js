851c5e8ca1dbd81c7d004d109edba51d937a6120
Gokul B G <59435818+Gokul-BG@users.noreply.github.com>
2020-07-13 16:07:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Registerpage from './Registerpage';
+import Loginpage from './Loginpage';
+ReactDOM.render(<Loginpage />, document.getElementById('root'));
+
